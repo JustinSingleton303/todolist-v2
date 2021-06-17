fca740b450744723ef8d0ba6d1e0706065db5149
JustinSingleton <jsingle9@msudenver.edu>
2021-06-17 14:30:32 -0600

    add schema

--- app.js
+++ app.js
@@ -3,16 +3,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
-
+const mongoose = require('mongoose');
 const app = express();
 
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+mongoose.connect('mongodb://localhost:27017/todoDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+const itemSchema = {
+  name: String
+};
+
+const Item = new mongoose.model("Item", itemSchema);
+
+//const items = ["Buy Food", "Cook Food", "Eat Food"];
+//const workItems = [];
 
 app.get("/", function(req, res) {
 
